dec1fffcc1733d4c46ad2dd4141fc893edb3780f
lcepy <xiangwenwe@foxmail.com>
2015-06-02 10:13:48 +0800

    初始化通知中心target

---
+++ manongweeklyExtension/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  manongweeklyExtension
+//
+//  Created by xiangwenwen on 15/6/2.
+//  Copyright (c) 2015年 xiangwenwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
---
+++ manongweeklyExtension/TodayViewController.m
@@ -0,0 +1,38 @@
+//
+//  TodayViewController.m
+//  manongweeklyExtension
+//
+//  Created by xiangwenwen on 15/6/2.
+//  Copyright (c) 2015年 xiangwenwen. All rights reserved.
+//
+
+#import "TodayViewController.h"
+#import <NotificationCenter/NotificationCenter.h>
+
+@interface TodayViewController () <NCWidgetProviding>
+
+@end
+
+@implementation TodayViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)widgetPerformUpdateWithCompletionHandler:(void (^)(NCUpdateResult))completionHandler {
+    // Perform any setup necessary in order to update the view.
+    
+    // If an error is encountered, use NCUpdateResultFailed
+    // If there's no update required, use NCUpdateResultNoData
+    // If there's an update, use NCUpdateResultNewData
+
+    completionHandler(NCUpdateResultNewData);
+}
+
+@end
